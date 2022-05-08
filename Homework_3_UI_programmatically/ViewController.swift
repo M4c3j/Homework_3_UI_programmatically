12dce9a6ef6a33420c115e427181e6e958fbe510
Maciej <maciejlipiecpl@gmail.com>
2022-05-08 11:24:24 +0200

    Top left menu button without transition.

--- Homework_3_UI_programmatically/ViewController.swift
+++ Homework_3_UI_programmatically/ViewController.swift
@@ -9,9 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	let screenHeight = UIScreen.main.bounds.height
+	let screenWidth = UIScreen.main.bounds.width
+	
+	let hamburgerMenu: UIButton = {
+		let button = UIButton(type: .system)
+		button.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+		button.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+		button.imageView?.centerYAnchor.constraint(equalTo: button.centerYAnchor).isActive = true
+		button.imageView?.centerXAnchor.constraint(equalTo: button.centerXAnchor).isActive = true
+		button.translatesAutoresizingMaskIntoConstraints = false
+		return button
+	} ()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		view.addSubview(hamburgerMenu)
+		
+		setupLayout()
+	}
+	
+	private func setupLayout() {
+		hamburgerMenu.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+		hamburgerMenu.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+		hamburgerMenu.heightAnchor.constraint(equalToConstant: 20).isActive = true
+		hamburgerMenu.widthAnchor.constraint(equalToConstant: 20).isActive = true
 	}
 
 
